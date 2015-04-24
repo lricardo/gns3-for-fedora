0b52a71a71d70a6ca69eaa44a9d228a378c59146
Leandro Ricardo <lricardo@github.com>
2015-04-24 15:39:19 +0100

    Fixed bad order of the script

--- aio-install.sh
+++ aio-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo ./install-dynamips.sh
 sudo ./install-gns3-server.sh
-sudo ./install-gns3-gui-shortcut.sh
 sudo ./install-gns3-gui.sh
+sudo ./install-gns3-gui-shorcut.sh
 sudo ./install-vpcs.sh
