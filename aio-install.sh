eb395304eb9ccdb4ad5fb9a29150695aba937136
Leandro Ricardo <leandro.j.c.ricardo@gmail.com>
2016-09-18 00:01:36 +0100

    Fixed typo

--- aio-install.sh
+++ aio-install.sh
@@ -3,5 +3,5 @@ sudo ./install-dynamips.sh
 sudo ./install-iouyap.sh
 sudo ./install-gns3-server.sh
 sudo ./install-gns3-gui.sh
-sudo ./install-gns3-gui-shorcut.sh
+sudo ./install-gns3-gui-shortcut.sh
 sudo ./install-vpcs.sh
